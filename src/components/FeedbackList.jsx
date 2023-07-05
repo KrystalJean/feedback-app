8b3e65a9fb7917f5f850da2d24c3cce35cbf4a06
kaustin <krystalaustin99@gmail.com>
2023-07-05 18:51:38 -0500

    added PropTypes

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,8 @@
-import FeedbackItem from "./FeedbackItem"
+import PropTypes from 'prop-types';
+import FeedbackItem from "./FeedbackItem";
 
-function FeedbackList({feedback}) {
-  if(!feedback || feedback.length === 0 ) {
+function FeedbackList({ feedback }) {
+  if (!feedback || feedback.length === 0) {
     return <p>No feedback yet.</p>
   }
 
@@ -14,6 +15,16 @@ function FeedbackList({feedback}) {
     </div>
 
   )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
 }
 
 export default FeedbackList
--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 function Card({children, reverse}) {
   // return (
   //   <div className={`card ${reverse && 'reverse'}`}>{children}</div>
@@ -17,6 +19,11 @@ function Card({children, reverse}) {
 
 Card.defaultProps = {
   reverse: true,
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
 }
 
 export default Card
